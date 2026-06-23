d5fd21c61f0ad86c7bfe539ea515c38b01f0f496
Habibullah <habibullahniaz00@gmail.com>
2026-06-23 19:15:41 +0500

    Update API baseUrl to live PythonAnywhere server

--- frontend/lib/utils/constants.dart
+++ frontend/lib/utils/constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
   // ---------------------------------------------------------------------------
   // BACKEND BASE URL
   // ---------------------------------------------------------------------------
-  static const String baseUrl = "http://192.168.1.3:8000/api";
+  static const String baseUrl = "https://smartreportingsystem.pythonanywhere.com/api";
 
   // ---------------------------------------------------------------------------
   // USER ROLES
